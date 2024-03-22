2629112756f909b1c46f5984f250c01371f6171b
MarchPy <joaomarchiori.jopa@gmail.com>
2024-03-22 10:48:55 -0300

    Updade

--- FiiHunter.py
+++ FiiHunter.py
@@ -1,18 +1,18 @@
 import os
 import json
 import requests
-import numpy as np
 import pandas as pd
 import requests_cache
-from io import StringIO
 from datetime import datetime
+from bs4 import BeautifulSoup
 from rich.console import Console
+
 
 
 class FiiHunter:
     def __init__(self) -> None:
         self._console = Console()
-        self.__header = {
+        self.__headers = {
             "Host": "fundamentus.com.br",
             "Sec-Ch-Ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
@@ -20,16 +20,16 @@ class FiiHunter:
             "Referer": "https://www.google.com/",
             "Accept-Language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7"
         }
-        self.__settings = self.__open_settings()
+        self.__settings = self.__openSettings()
 
-    def __open_settings(self) -> dict:
+    def __openSettings(self) -> dict:
         try:
             with open(file='config/settings.json', mode='r', encoding='utf-8') as fileObj:
                 settings = json.load(fp=fileObj)
                 return settings
 
         except FileNotFoundError:
-            self._console.print(f'[{self.__time()}] -> [[italic red]Arquivo settings.json não foi encontrado.[/]]')
+            self._console.print(f'[{self.__time()}] -> [[italic red]Settings file not found.[/]]')
 
     @staticmethod
     def __time() -> str:
@@ -40,108 +40,124 @@ class FiiHunter:
         return datetime.now().strftime(format='%d-%m-%Y')
 
     def fundamentus(self) -> pd.DataFrame:
-        df_fundamentus = pd.DataFrame()
+        data = []
+        dataNotFound = []
         symbols = self.__settings['fundos']
 
         idx = 1
         with requests_cache.enabled('config/cache.db'):
             for symbol in symbols:
                 symbol = symbol.upper()
-                self._console.print(f'[{self.__time()}] -> [[italic yellow]Coletando dados fundamentalistas para o ativo[/]]-[{idx} de {len(symbols)}] :: {symbol} -> ', end='')
+                self._console.print(f'[{self.__time()}] -> [[italic yellow]Collecting fundamental data for ticker[/]]-[{idx} of {len(symbols)}] :: {symbol} -> ', end='')
 
                 url = f"http://fundamentus.com.br/detalhes.php?papel={symbol}"
+                content = requests.get(url=url, headers=self.__headers)
 
-                try:
-                    content = requests.get(url, headers=self.__header)
+                if content.status_code == 200:
+                    soup = BeautifulSoup(content.content, 'html.parser')
 
-                    tables_html = pd.read_html(io=StringIO(content.text), decimal=',', thousands='.')
-                    df_0 = tables_html[0]
-                    df_1 = tables_html[1]
-                    df_2 = tables_html[2]
-                    df_3 = tables_html[3]
-                    df_4 = tables_html[4]
-                  
-                    df_final = pd.concat(objs=[
-                            pd.concat(objs=[pd.concat([df_0[0], df_0[2]]), pd.concat([df_0[1], df_0[3]])], axis=1).T,
-                            pd.concat(objs=[pd.concat([df_1[0], df_1[2]]), pd.concat([df_1[1], df_1[3]])], axis=1).T,
-                            pd.concat(objs=[pd.concat([df_2[2], df_2[4]]), pd.concat([df_2[3], df_2[5]])], axis=1).T.drop(columns=0),
-                            pd.concat(objs=[pd.concat([df_3[0], df_3[2]]), pd.concat([df_3[1], df_3[3]])], axis=1).T.drop(columns=0),
-                            pd.concat(objs=[pd.concat([df_4[0], df_4[2], df_4[4]]), pd.concat([df_4[1], df_4[3], df_4[5]])], axis=1).T.drop(columns=0),
-                            pd.concat(objs=[df_2[0], df_2[1]], axis=1).T.drop(columns=0)[[1, 2, 3, 4]]
+                    if soup.find(string='Nenhum papel encontrado') is None:
+                        tables = soup.find(name='div', attrs={'class': 'conteudo clearfix'})
+                        tables = soup.find_all(name='table', attrs={'class': 'w728'})
 
-                        ], axis=1)
+                        # --- Proper way to identify the values ---
 
-                    df_final.columns = df_final.iloc[0].str.replace(pat='?', repl='')
+                        # numTable = 2
+                        # print("\n\n")
+                        # print(f'Contains -{len(tables)}- tables.')
+                        # i = 0
+                        # for index, row in zip(tables[numTable].find_all(name='td', attrs={'class': 'label'}), tables[numTable].find_all(name='td', attrs={'class': 'data'})):
+                        #     print(i, index)
+                        #     print(i, row)
+                        #     print('\n')
+                        #     i += 1
+                        # input("\n\n")
 
-                    line = df_final[1:2]
+                        # ----------------------------------------------------
 
-                    if not line.empty:
-                        df_fundamentus = pd.concat([df_fundamentus, line])
-                        self._console.print('[[green]Dados fundamentalistas coletados[/]]')
+                        # Tabela 0
+                        ativo = tables[0].find_all(name='td', attrs={'class': 'data'})[0].text
+                        preco = float(tables[0].find_all(name='td', attrs={'class': 'data'})[1].text.replace('.', '').replace(',', '.'))
+                        nome = tables[0].find_all(name='td', attrs={'class': 'data'})[2].text
+                        precoMinimo52Sem = float(tables[0].find_all(name='td', attrs={'class': 'data'})[5].text.replace('.', '').replace(',', '.'))
+                        segmento = tables[0].find_all(name='td', attrs={'class': 'data'})[6].text
+                        precoMaximo52Sem = float(tables[0].find_all(name='td', attrs={'class': 'data'})[7].text.replace('.', '').replace(',', '.'))
+                        volumeMedio2m = int(tables[0].find_all(name='td', attrs={'class': 'data'})[9].text.replace('.', ''))
 
-                except (ValueError, IndexError):
-                    self._console.print('[[red]Não foi possível coletar os dados fundamentalistas[/]]')
+                        # Tabela 1
+                        nTotalCotas = tables[1].find_all(name='td', attrs={'class': 'data'})[1].text
+                        relatorio = tables[1].find_all(name='td', attrs={'class': 'data'})[2].find(name='a')
+                        relatorio = relatorio['href'] if relatorio is not None else "-"
+
+                        # Tabela 2
+                        dividendYield = tables[2].find_all(name='td', attrs={'class': 'data'})[4].text
+                        dividendYield = float(dividendYield.replace('%', '').replace('.', '').replace(',', '.')) if dividendYield != '-' else 0
+                        pvp = float(tables[2].find_all(name='td', attrs={'class': 'data'})[7].text.replace(',', '.'))
+                        vpCota = tables[2].find_all(name='td', attrs={'class': 'data'})[8].text
+                        vpCota = float(vpCota.replace('.', '').replace(',', '.')) if vpCota != '-' else 0
+                        dividendoCota = tables[2].find_all(name='td', attrs={'class': 'data'})[5].text
+                        dividendoCota = float(dividendoCota.replace('.', '').replace(',', '.')) if dividendoCota != '-' else 0
+                        ffoCota = tables[2].find_all(name='td', attrs={'class': 'data'})[5].text
+                        ffoCota = float(ffoCota.replace('.', '').replace(',', '.')) if ffoCota != '-' else 0
+                        ffoYield = tables[2].find_all(name='td', attrs={'class': 'data'})[1].text
+                        ffoYield = float(ffoYield.replace('%', '').replace('.', '').replace(',', '.')) if ffoYield != '-' else 0
+                        ativos = int(tables[2].find_all(name='td', attrs={'class': 'data'})[25].text.replace('.', ''))
+                        patrimLiq = int(tables[2].find_all(name='td', attrs={'class': 'data'})[26].text.replace('.', ''))
+
+                        # Tabela 4
+                        qtdImoveis = int(tables[4].find_all(name='td', attrs={'class', 'data'})[0].text)
+                        qtdUnidades = tables[4].find_all(name='td', attrs={'class', 'data'})[3].text
+                        qtdUnidades = int(qtdUnidades) if qtdUnidades != '' else 0
+                        areaM2 = tables[4].find_all(name='td', attrs={'class', 'data'})[1].text
+                        capRate = tables[4].find_all(name='td', attrs={'class', 'data'})[2].text
+                        vacMedia = tables[4].find_all(name='td', attrs={'class', 'data'})[5].text
+                        vacMedia = float(vacMedia.replace('%', '').replace(',', '.')) if vacMedia != '-' else 0
+                        precoM2 = tables[4].find_all(name='td', attrs={'class', 'data'})[7].text
+
+                        data.append(
+                            [
+                                ativo, segmento, nome, preco, precoMinimo52Sem, precoMaximo52Sem, volumeMedio2m, ativos, patrimLiq, nTotalCotas, dividendYield,
+                                dividendoCota, pvp, vpCota, ffoCota, ffoYield, qtdImoveis, qtdUnidades, areaM2, capRate, vacMedia, precoM2,
+                                relatorio
+                            ]
+                        )
+
+
+                        self._console.print('[[bold green]Data collected successfully[/]]')
+
+                    else:
+                        self._console.print('[[bold red]Data not found[/]]')
+                        dataNotFound.append(symbol)
+
+                else:
+                    self._console.print(f"Error code: {content.status_code}")
+                    continue
 
                 idx += 1
-                
-        columns_to_drop = [
-            np.nan, 'Data últ cot', 'Balanço Patrimonial', 'Últimos 3 meses', 'Balanço Patrimonial', 'Últimos 12 meses', 'Receita', 'FFO', 'Resultado', 'Últ Info Trimestral', 'Relatório',
-            'Mandato', 'Gestão', 'Venda de ativos', 'Rend. Distribuído'
+
+        os.system(command='cls' if os.name == 'nt' else 'clear')
+        if dataNotFound:
+            self._console.print(f'\n[[bold yellow]Unable to collect data from {len(dataNotFound)} ticker(s)[/]] -> {dataNotFound}')
+            with open(f'Tickers without data ({self.__date()}).txt', 'w') as fileObj:
+                fileObj.write(str(dataNotFound))
+
+        columns = [
+            'Ativo', 'Segmento', 'Nome', 'Cotação', 'Preço 52sem (Min)', 'Preço 52sem (Max)', 'Vol $ méd (2m)', 'Ativos',
+            'Patrim. Líquido', 'N° total de cotas', 'Div. Yield', 'Dividendo/cota', 'P/VP', 'VP/Cota', 'FFO/Cota', 'FFO Yield',
+            'Qtd. Imóveis', 'Qtd. Unidades', 'Área (m2)', 'Cap Rate', 'Vacância média', 'Preço do m2', 'Relatório'
         ]
-        columns_to_int = [
-            'Vol $ méd (2m)', 'Valor de mercado', 'Patrim Líquido', 'Nro. Cotas', 'Ativo', 'Venda de ativos', 
-            'Qtd imóveis', 'Qtd Unidades', 'Área (m2)', 'Aluguel/m2', 'Preço do m2'
-        ]
-        columns_to_float = [
-            'Cotação', 'Min 52 sem', 'Max 52 sem', 'P/VP', 'FFO/Cota', 'Dividendo/cota', 'VP/Cota',
-            'FFO/Cota'
-        ]
-        columns_to_format_float = [
-            'FFO Yield', 'Vacância Média', 'Cap Rate', 'Imóveis/PL do FII', 'Div. Yield', 'Dia', 'Mês', '30 dias', '12 meses'
-        ]
-
-        df_fundamentus = df_fundamentus.replace(to_replace=r'^-$', value='0', regex=True)
-        df_fundamentus.fillna(value=0, inplace=True)
-
-        for to_drop in columns_to_drop:
-            try:
-                df_fundamentus.drop(columns=[to_drop], inplace=True)
-
-            except KeyError:
-                pass
-
-        for to_int in columns_to_int:
-            try:
-                df_fundamentus[to_int] = df_fundamentus[to_int].astype(np.int64)
-
-            except KeyError:
-                pass
-
-        for to_float in columns_to_float:
-            try:
-                df_fundamentus[to_float] = df_fundamentus[to_float].astype(float)
-
-            except KeyError:
-                pass
-
-        for to_format_float in columns_to_format_float:
-            try:
-                df_fundamentus[to_format_float] = df_fundamentus[to_format_float].astype(str).str.replace(pat='.', repl='').str.replace(pat=',', repl='.').str.replace(pat='%', repl='').astype(float)
-
-            except KeyError:
-                pass
-
-        return df_fundamentus.reset_index(drop=True)
+        dfFundamentus = pd.DataFrame(data=data, columns=columns)
+        return dfFundamentus
 
     def filter(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         filter_ = (
             (dataframe['Div. Yield'] >= self.__settings['filtro']['Dividend yield (Min)']) &
             (dataframe['Vol $ méd (2m)'] > self.__settings['filtro']['Volume médio 2m (Min)']) &
-            (dataframe['Qtd imóveis'] >= self.__settings['filtro']['Quantidade de imóveis (Min)']) &
+            (dataframe['Qtd. Imóveis'] >= self.__settings['filtro']['Quantidade de imóveis (Min)']) &
             (dataframe['P/VP'] < self.__settings['filtro']['P/VP (Max)']) &
             (dataframe['P/VP'] > self.__settings['filtro']['P/VP (Min)']) &
             (dataframe['VP/Cota'] > dataframe['Cotação'] if self.__settings['filtro']['VP/Cota > Preço cota'] else None) &
-            (dataframe['Vacância Média'] < self.__settings['filtro']['Vacância média (Max)'])
+            (dataframe['Vacância média'] < self.__settings['filtro']['Vacância média (Max)'])
         )
 
         return dataframe[filter_]
@@ -154,9 +170,9 @@ class FiiHunter:
         except FileExistsError:
             pass
 
-        filename = f"Resultado dos fundos imobiliários ({self.__date()}).xlsx"
+        filename = f"FII Results ({self.__date()}).xlsx"
         dataframe.to_excel(excel_writer=os.path.join(folder, filename), index=False)
-        self._console.print(f'[[bold yellow]Resultado salvo em excel[/]] -> ({os.path.join(folder, filename)})')
+        self._console.print(f'[[bold yellow]Result saved to excel[/]] -> ({os.path.join(folder, filename)})')
 
     def ranking(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         dataframe = dataframe.copy()
@@ -172,8 +188,6 @@ class FiiHunter:
 
         return dataframe.sort_values(by='Ranking', ascending=False)
 
-    def display_result(self, dataframe: pd.DataFrame) -> None:
-        os.system(command='cls' if os.name == 'nt' else 'clear')
-        
+    def displayResult(self, dataframe: pd.DataFrame) -> None:
         self._console.print(f'\n\n[{self.__time()}] -> [[italic bold green]Resultado final resumido[/]]:')
-        self._console.print(dataframe[['FII', 'Nome', 'Segmento', 'Cotação', 'Div. Yield', 'P/VP', 'VP/Cota', 'Dividendo/cota', 'Qtd imóveis', 'Qtd Unidades', 'Vacância Média', 'Ranking']].to_string(index=False) + "\n" if not dataframe.empty else "[bold red]Nenhuma oportunidade encontrada[/]\n")
+        self._console.print(dataframe[['Ativo', 'Nome', 'Segmento', 'Cotação', 'Div. Yield', 'P/VP', 'VP/Cota', 'Dividendo/cota', 'Qtd. Imóveis', 'Qtd. Unidades', 'Vacância média', 'Ranking']].to_string(index=False) + "\n" if not dataframe.empty else "[bold red]Nenhuma oportunidade encontrada[/]\n")
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def main():
     df_fundamentus = app.fundamentus()
     df_filtered = app.filter(dataframe=df_fundamentus)
     df_ranking = app.ranking(dataframe=df_filtered)
-    app.display_result(dataframe=df_ranking)
+    app.displayResult(dataframe=df_ranking)
     if "-s" in argv:
         app.save_file(dataframe=df_ranking)
 
